c64eb9acb16770c5666fd4a64fa56bea1a81bde8
Vass Máté <vass.mate99@gmail.com>
2019-09-20 23:29:00 +0200

    offline works now, allow access bug after installation, have to put the map file on the phone to the correct directory manually

--- app/src/main/java/com/example/osm/MainActivity.java
+++ app/src/main/java/com/example/osm/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.osm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
@@ -11,40 +10,77 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
-import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.simplefastpoint.LabelledGeoPoint;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
 
-import static android.Manifest.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_STORAGE = 1000;
+
     MapView map = null;
+    TextView test_text;
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
 
+        }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Context ctx = getApplicationContext();
         Configuration.getInstance().setUserAgentValue(getPackageName());
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
         map = (MapView) findViewById(R.id.mapview);
-        map.setTileSource(TileSourceFactory.MAPNIK);
+        test_text = (TextView) findViewById(R.id.test_text);
+
+        Configuration.getInstance().setOsmdroidBasePath((new File( "storage/emulated/0/osmdroid")));
+        //Configuration.getInstance().setOsmdroidTileCache((new File("storage/emulated/0/osmdroid/tiles")));
+
+        //map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setUseDataConnection(false);
+        XYTileSource tileSource = new XYTileSource(
+                "4uMaps",
+                10,
+                15,
+                256,
+                ".png",
+                new String[] {});
+        map.setTileSource(tileSource);
+
+        test_text.setText(Configuration.getInstance().getOsmdroidBasePath().toString());
+
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
         IMapController mapController = map.getController();
-        mapController.setZoom(9.5);
+        mapController.setZoom(12);
         GeoPoint startPoint = new GeoPoint(46.253, 20.1414);
         mapController.setCenter(startPoint);
     }
