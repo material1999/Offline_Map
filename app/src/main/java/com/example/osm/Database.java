6bfb52e9c8ab70ab79074e1a0a48d46f5c3bbdd5
Vass Máté <vass.mate99@gmail.com>
2019-10-11 01:01:13 +0200

    started the database, added multiple markers, assets functionality

---
+++ app/src/main/java/com/example/osm/Database.java
@@ -0,0 +1,72 @@
+package com.example.osm;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Database extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "places.db";
+    public static final String TABLE_NAME = "places_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "COORDINATES_X";
+    public static final String COL_4 = "COORDINATES_Y";
+    public static final String COL_5 = "TYPE";
+    public static final String COL_6 = "DESCRIPTION";
+
+
+    public Database(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,COORDINATES_X REAL," +
+                "COORDINATES_Y TEXT,TYPE TEXT,DESCRIPTION TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public SQLiteDatabase addData(InputStreamReader is, Context c) {
+        c.deleteDatabase(DATABASE_NAME);
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        String row;
+        try {
+            BufferedReader reader = new BufferedReader(is);
+            while ((row = reader.readLine()) != null) {
+                String[] data = row.split(";");
+                contentValues.put(COL_2, data[0]);
+                contentValues.put(COL_3, data[1]);
+                contentValues.put(COL_4, data[2]);
+                contentValues.put(COL_5, data[3]);
+                contentValues.put(COL_6, data[4]);
+                db.insert(TABLE_NAME,null, contentValues);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return db;
+    }
+
+    public Cursor getCursor() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+}
--- app/src/main/java/com/example/osm/MainActivity.java
+++ app/src/main/java/com/example/osm/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,25 +25,37 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.events.MapListener;
 import org.osmdroid.events.ScrollEvent;
 import org.osmdroid.events.ZoomEvent;
+import org.osmdroid.tileprovider.tilesource.BitmapTileSourceBase;
 import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import static org.osmdroid.tileprovider.util.StreamUtils.copy;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST = 1;
 
+    Database myDatabase;
     MapView map;
     TextView test_text;
     Button test_button;
-    Context myContext;
+    Context c;
     MyLocationNewOverlay myLocationOverlay;
     LocationManager myLocationManager;
     IMapController mapController;
@@ -54,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onProviderEnabled(String s) {
-            test_text.setText("enabled");
             myLocationOverlay.enableFollowLocation();
             myLocationOverlay.enableMyLocation(myLocationProvider);
             map.getOverlays().add(1, myLocationOverlay);
@@ -62,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onProviderDisabled(String s) {
-            test_text.setText("disabled");
             myLocationOverlay.disableFollowLocation();
             myLocationOverlay.disableMyLocation();
             map.getOverlays().remove(myLocationOverlay);
@@ -72,8 +86,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        test_text.setText("resume");
-        //myLocationOverlay.enableFollowLocation();
         myLocationOverlay.enableMyLocation(myLocationProvider);
         map.getOverlays().add(1, myLocationOverlay);
     }
@@ -81,7 +93,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onPause() {
         super.onPause();
-        test_text.setText("pause");
         myLocationOverlay.disableFollowLocation();
         myLocationOverlay.disableMyLocation();
         map.getOverlays().remove(myLocationOverlay);
@@ -89,6 +100,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void initializeMyMap () {
         map.setUseDataConnection(false);
+        File tileDir = Configuration.getInstance().getOsmdroidBasePath();
+        File tileZipFile = new File(tileDir, "test_map.zip");
+        if (!tileZipFile.exists()){
+            try{
+                InputStream in = getAssets().open("test_map.zip");
+                OutputStream out = new FileOutputStream(tileZipFile);
+                copy(in,out);
+                in.close();
+                //in = null;
+                out.flush();
+                out.close();
+                //out = null;
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
         XYTileSource tileSource = new XYTileSource(
                 "4uMaps",
                 10,
@@ -105,10 +132,11 @@ public class MainActivity extends AppCompatActivity {
         mapController.setZoom(12.0);
     }
 
+    @SuppressLint("MissingPermission")
     public void initializeMyGPS() {
         myLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
-        myLocationProvider = new GpsMyLocationProvider(myContext);
+        myLocationProvider = new GpsMyLocationProvider(c);
         myLocationOverlay = new MyLocationNewOverlay(myLocationProvider, map);
     }
 
@@ -117,14 +145,24 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myContext = getApplicationContext();
+        c = getApplicationContext();
         Configuration.getInstance().setUserAgentValue(getPackageName());
-        Configuration.getInstance().load(myContext, PreferenceManager.getDefaultSharedPreferences(myContext));
+        Configuration.getInstance().load(c, PreferenceManager.getDefaultSharedPreferences(c));
 
-        Configuration.getInstance().setOsmdroidBasePath((new File( "storage/emulated/0/osmdroid")));
-        Configuration.getInstance().setOsmdroidTileCache((new File("storage/emulated/0/osmdroid/tiles")));
+        test_text = (TextView) findViewById(R.id.test_text);
+        String fileName = "data.csv";
+        myDatabase = new Database(this);
+        SQLiteDatabase db = null;
+        try {
+            InputStreamReader is = new InputStreamReader(getAssets().open(fileName));
+            db = myDatabase.addData(is, c);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Cursor cursor = myDatabase.getCursor();
 
-        String[] permissions = new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION};
+        String[] permissions = new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             for (String p: permissions) {
                 if (ActivityCompat.checkSelfPermission(this, p) != PackageManager.PERMISSION_GRANTED) {
@@ -136,7 +174,6 @@ public class MainActivity extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-            test_text = (TextView) findViewById(R.id.test_text);
             map = (MapView) findViewById(R.id.mapview);
             test_button = (Button) findViewById(R.id.test_button);
             test_button.setOnClickListener(new View.OnClickListener() {
@@ -178,12 +215,29 @@ public class MainActivity extends AppCompatActivity {
             GeoPoint startPoint = new GeoPoint(46.253, 20.1414);
             mapController.setCenter(startPoint);
 
-            GeoPoint testPoint = new GeoPoint(46.246708, 20.151135);
-            Marker startMarker = new Marker(map);
-            startMarker.setPosition(testPoint);
-            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-            map.getOverlays().add(0, startMarker);
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(1);
+                Double coordinatesX = cursor.getDouble(2);
+                Double coordinatesY = cursor.getDouble(3);
+                String type = cursor.getString(4);
+                String description = cursor.getString(5);
+                Marker marker = new Marker(map);
+                marker.setPosition(new GeoPoint(coordinatesX, coordinatesY));
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
 
+
+
+
+
+
+
+
+
+
+
+                map.getOverlays().add(marker);
+            }
+            map.getOverlays().add(myLocationOverlay);
         } else {
             System.exit(1);
         }
